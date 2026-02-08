e2af98d8cceb3e513ce0f3a0488b267c2a925df8
Misak Sofoyan <misaksofoyan@gmail.com>
2026-02-08 18:36:13 +0400

    test

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,7 +4,7 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
-  testEnvironment: "jsdom",
+  testEnvironment: "node",
   transform: {
     ...tsJestTransformCfg,
   },
